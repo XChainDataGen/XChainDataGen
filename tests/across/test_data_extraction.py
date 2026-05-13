b385beecfc2cea0b2c3d7b07d78c8cb92f852968
Rafael Ribeiro <100279653+RafaR13@users.noreply.github.com>
2026-05-13 15:51:34 +0100

    fix: across test times (#7)
    Changed test times to 1/12/2025 in order to avoid outdated event names.
    Additionally, changed the number of expected events on each table
    according to 3 local runs.
    
    Signed-off-by: Rafael Ribeiro <rafael.m.ribeiro@tecnico.ulisboa.pt>

--- tests/across/test_data_extraction.py
+++ tests/across/test_data_extraction.py
@@ -7,8 +7,8 @@ def test_extract_data():
     args = argparse.Namespace(
         blockchains=["ethereum", "arbitrum", "polygon", "optimism", "base", "scroll", "linea"],
         bridge="across",
-        start_ts=1733011200,  # 1st Dec 2024 00:00
-        end_ts=1733014800,  # 1st Dec 2024 01:00
+        start_ts=1764547200,  # 1st Dec 2025 00:00
+        end_ts=1764550800,  # 1st Dec 2025 01:00
     )
 
     Cli.extract_data(args)
@@ -28,20 +28,20 @@ def test_extract_data():
 
     events = across_funds_deposited.get_all()
     print(f"Number of events in AcrossFundsDeposited: {len(events)}")
-    assert len(events) == 911, (
-        "Expected 911 events in AcrossFundsDepositedRepository table after extraction."
+    assert len(events) == 447, (
+        "Expected 447 events in AcrossFundsDepositedRepository table after extraction."
     )
 
     events = across_filled_relay_repo.get_all()
     print(f"Number of events in AcrossFilledRelay: {len(events)}")
-    assert len(events) == 912, (
-        "Expected 912 events in AcrossFilledRelayRepository table after extraction."
+    assert len(events) == 446, (
+        "Expected 446 events in AcrossFilledRelayRepository table after extraction."
     )
 
     events = across_relayer_refund_repo.get_all()
     print(f"Number of events in AcrossRelayerRefund: {len(events)}")
-    assert len(events) == 93, (
-        "Expected 93 events in AcrossRelayerRefundRepository table after extraction."
+    assert len(events) == 128, (
+        "Expected 128 events in AcrossRelayerRefundRepository table after extraction."
     )
 
     args = argparse.Namespace(
@@ -57,6 +57,6 @@ def test_extract_data():
     transactions = across_cross_chain_transactions_repo.get_all()
 
     print(f"Number of transactions in AcrossCrossChainTransaction: {len(transactions)}")
-    assert len(transactions) == 900, (
-        "Expected 900 events in AcrossCrossChainTransaction table after extraction."
+    assert len(transactions) == 445, (
+        "Expected 445 events in AcrossCrossChainTransaction table after extraction."
     )
